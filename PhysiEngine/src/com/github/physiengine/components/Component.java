07d7f525b70327643de2b314b1df62fb7470e8e0
Florian <Florian@129.217.100.250>
2018-11-13 11:02:19 +0100

    Added methods to GameObject, Components

--- PhysiEngine/src/com/github/physiengine/components/Component.java
+++ PhysiEngine/src/com/github/physiengine/components/Component.java
@@ -1,17 +1,33 @@
 package com.github.physiengine.components;
 
+import com.github.physiengine.object.*;
+
 public abstract class Component {
 
 	private String name;
-	
+
 	private GameObject parent;
-	
+
 	public Component(String name) {
-		
+
 		this.name = name;
-		
+
 	}
-	
+
 	public abstract void update();
+
+	public GameObject getParent() {
+		return parent;
+	}
+
+	public void setParent(GameObject parent) {
+		this.parent = parent;
+	}
+
+	public String getName() {
+
+		return name;
+
+	}
 
 }
--- PhysiEngine/src/com/github/physiengine/object/GameObject.java
+++ PhysiEngine/src/com/github/physiengine/object/GameObject.java
@@ -1,8 +1,32 @@
 package com.github.physiengine.object;
 
+import java.util.HashMap;
+
+import com.github.physiengine.components.Component;
+
 public class GameObject {
 
+	private HashMap<String, Component> components;
+	
 	public GameObject() {
+		
+		components = new HashMap<>();
+		
+	}
+	
+	public boolean addComponent(Component c) {
+		
+		if(components.containsKey(c.getName())) {
+			
+			return false;
+			
+		}
+		
+		else {
+			
+			components.put(c.getName(), c);
+			return true;
+		}
 		
 	}
 
