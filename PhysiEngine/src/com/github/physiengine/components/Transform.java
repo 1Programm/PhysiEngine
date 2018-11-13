51d972f66f4acfa9adba1b59214ac522ebddbd2a
Florian <Florian@129.217.100.250>
2018-11-13 11:08:44 +0100

    Added class Transform.java

---
+++ PhysiEngine/src/com/github/physiengine/components/Transform.java
@@ -0,0 +1,23 @@
+package com.github.physiengine.components;
+
+import com.github.physiengine.math.Vector2;
+
+public class Transform extends Component {
+
+	private Vector2 pos, scale;
+	
+	private float rotation;
+	
+	public Transform(String name) {
+		super(name);
+		
+	}
+
+	@Override
+	public void update() {
+		
+		
+
+	}
+
+}
