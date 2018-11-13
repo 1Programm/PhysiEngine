8537e0f81d9c7af15b801c613db97e8642300d98
Programm <1Pro2gra3mm45@web.de>
2018-11-13 11:08:55 +0100

    Added Renderer Component

---
+++ PhysiEngine/src/com/github/physiengine/components/Renderer.java
@@ -0,0 +1,14 @@
+package com.github.physiengine.components;
+
+public class Renderer extends Component{
+
+	public Renderer(String name) {
+		super(name);
+	}
+
+	@Override
+	public void update() {
+		
+	}
+
+}
