568be7e3b08bb145f917e530fac4c4860f6690af
Programm <1Pro2gra3mm45@web.de>
2018-11-20 10:56:36 +0100

    Circle setup

--- PhysiEngine/src/com/github/physiengine/components/collision/Circle.java
+++ PhysiEngine/src/com/github/physiengine/components/collision/Circle.java
@@ -1,7 +1,13 @@
 package com.github.physiengine.components.collision;
 
+import com.github.physiengine.math.Vector2;
+
 public class Circle extends Collider{
 
+	public Vector2 position;
+	
+	public float radius;
+	
 	@Override
 	protected CollisionInfo collideWith(AABB aabb) {
 		// TODO Auto-generated method stub
