757623d23b910af91327e61d39ac433d8a9ad0cd
Programm 1 <julian.becker.berghofen@gmx.de>
2018-11-21 17:03:02 +0200

    test2.2

--- PhysiEngine/src/com/github/physiengine/math/Vector2.java
+++ PhysiEngine/src/com/github/physiengine/math/Vector2.java
@@ -54,14 +54,14 @@ public class Vector2 {
 	public void add1() {
 		
 	}
+	
+	public void add2() {
+		
+	}
 
 	public void sub(Vector2 sv) {
 		this.x -= sv.x;
 		this.y -= sv.y;
-	}
-	
-	public void add2() {
-		
 	}
 	
 	public void div(Vector2 sv) {
