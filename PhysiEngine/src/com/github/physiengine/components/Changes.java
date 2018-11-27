74444d0ff29c4bc9f3166faec6e5bfc5a60e4bdd
Florian <deinemudda@peeeeeenis.xxx>
2018-11-27 10:36:59 +0100

    lol

---
+++ PhysiEngine/src/com/github/physiengine/components/Changes.java
@@ -0,0 +1,9 @@
+package com.github.physiengine.components;
+
+public class Changes {
+
+	public boolean position;
+	public boolean size;
+	public boolean rotation;
+	
+}
---
+++ PhysiEngine/src/com/github/physiengine/components/model/Model.java
@@ -0,0 +1,20 @@
+package com.github.physiengine.components.model;
+
+import com.github.physiengine.components.Changes;
+import com.github.physiengine.components.Component;
+import com.github.physiengine.math.Vector2;
+
+public class Model extends Component{
+
+	protected Vector2[] vertecies;
+	
+	public Model() {
+		super("Model");
+	}
+
+	@Override
+	public void update(Changes c) {
+		
+	}
+
+}
