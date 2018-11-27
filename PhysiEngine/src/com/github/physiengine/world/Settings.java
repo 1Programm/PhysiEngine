3c271528ae260f3c267f09879841087592da3a34
Programm <1Pro2gra3mm45@web.de>
2018-11-27 23:09:01 +0100

    Small changes to Material and Setting. Deleted PhysicsBody for now...

--- PhysiEngine/src/com/github/physiengine/world/Settings.java
+++ PhysiEngine/src/com/github/physiengine/world/Settings.java
@@ -1,12 +1,11 @@
 package com.github.physiengine.world;
 
 import com.github.physiengine.math.Vector2;
-import com.github.physiengine.object.Material;
 
 public class Settings {
 
-	public static Vector2 GRAVITY = new Vector2();
+	public static Vector2 GRAVITY = new Vector2(0, -0.8f);
 
-	public static Material SPACE_MATERIAL = new Material(0, 0, 0.1f); 
+	public static float airFriction = 0.2f; 
 	
 }
