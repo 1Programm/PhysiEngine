b74fe4a9c54ae8e40b70b9dc3798271643a0b0df
1Programm <1Pro2gra3mm45@web.de>
2018-11-28 11:15:19 +0100

    Adding imports

--- PhysiEngine/src/com/github/helperclasses/strings/StringUtils.java
+++ PhysiEngine/src/com/github/helperclasses/strings/StringUtils.java
@@ -1,18 +1,17 @@
 package src.com.github.helperclasses.strings;
 
-import java.utils.HashMap;
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
 
 public class StringUtils {
-
- private static HashMap<String, Integer> keyboardKeys = new HashMap<>();
- 
- static {
-  keyboardKeys.put("Q", KeyEvent.VK_Q),
-  keyboardKeys.put("W", KeyEvent.VK_W),
- };
-
- public static int KeyCharToKeyCode(String keyChar) {
-  return keyboardKeys.get(keyChar);
- }
-
+	private static HashMap<String, Integer> keyboardKeys = new HashMap<>();
+	
+	static {
+		keyboardKeys.put("Q", KeyEvent.VK_Q),
+		keyboardKeys.put("W", KeyEvent.VK_W),
+	};
+	
+	public static int KeyCharToKeyCode(String keyChar) {
+		return keyboardKeys.get(keyChar);
+	}
 }
